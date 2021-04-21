4cd706d5f61ac247e45f164088ddbc68bfe24fd3
Gustavo Paco <gustavopaco@gmail.com>
2021-04-21 20:55:47 -0300

    Interface Login Usuario Implementada

--- homepets/app/src/main/java/br/com/cotemig/homepets/ui/activities/HomeActivity.kt
+++ homepets/app/src/main/java/br/com/cotemig/homepets/ui/activities/HomeActivity.kt
@@ -2,11 +2,27 @@ package br.com.cotemig.homepets.ui.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import br.com.cotemig.homepets.R
+import br.com.cotemig.homepets.models.Pessoa
+import br.com.cotemig.homepets.models.User
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+    var p = Pessoa()
+        p.nome = "Gustavo"
+        p.cpf = "107.775.376-48"
+        Toast.makeText(this,"Email: " + p.getmail(),Toast.LENGTH_LONG).show()
+        Toast.makeText(this,"Nome: " + p.nome + "\nCPF: " + p.cpf,Toast.LENGTH_LONG).show()
+
+        p.setmail("gustavopaco@gmail.com")
+        Toast.makeText(this,"Nome: " + p.getmail(),Toast.LENGTH_LONG).show()
+
+
+
+
     }
 }
---
+++ homepets/app/src/main/java/br/com/cotemig/homepets/ui/activities/LoginUserActivity.kt
@@ -0,0 +1,12 @@
+package br.com.cotemig.homepets.ui.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.cotemig.homepets.R
+
+class LoginUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_user)
+    }
+}
