8ed149f6f2c60fb88d79167d4626f5ed9a8d997e
Rodrigo <70204160@aluno.faculdadecotemig.br>
2021-04-22 21:41:53 -0300

    Mock Api Login

---
+++ backend/HomePetsAPI/HomePets.Domain/DTO/AuthModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HomePets.Domain
+{
+    public class AuthModel
+    {
+        public string cpf { get; set; }
+        public string password { get; set; }
+    }
+
+}
---
+++ backend/HomePetsAPI/HomePets.Domain/DTO/RegisterModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HomePets.Domain
+{
+    public class RegisterModel
+    {
+        public string cpf { get; set; }
+        public string nome { get; set; }
+        public string telefone { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+
+        public string datanascimento { get; set; }
+        public string genero { get; set; }
+    }
+
+}
