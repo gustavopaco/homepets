9862f4928159790ff1ef4cf38c22fdff4b15c1ec
Gustavo Paco <gustavopaco@gmail.com>
2021-04-23 14:39:38 -0300

    Implementando back-end User Cadastro e validando.

--- homepets/app/src/main/java/br/com/cotemig/homepets/ui/activities/LoginUserActivity.kt
+++ homepets/app/src/main/java/br/com/cotemig/homepets/ui/activities/LoginUserActivity.kt
@@ -1,12 +1,25 @@
 package br.com.cotemig.homepets.ui.activities
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import br.com.cotemig.homepets.R
+import kotlinx.android.synthetic.main.activity_login_user.*
 
 class LoginUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_user)
+
+        btn_registrar.setOnClickListener {
+            goRegisterActivity()
+        }
+
     }
+
+    fun goRegisterActivity(){
+        var intent = Intent(this,RegisterActivity::class.java)
+        startActivity(intent)
+    }
+
 }
