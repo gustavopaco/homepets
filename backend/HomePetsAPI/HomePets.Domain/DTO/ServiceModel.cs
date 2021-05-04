067813f224c21c3a37387ee1eb909c9b83b02e11
Rodrigo Rocha <rodrigo@rochadigital.com>
2021-05-03 22:51:20 -0300

    Ajustando MOCK da API

--- backend/HomePetsAPI/HomePets.Domain/DTO/ServiceModel.cs
+++ backend/HomePetsAPI/HomePets.Domain/DTO/ServiceModel.cs
@@ -4,11 +4,9 @@ namespace HomePets.Domain
 {
     public class ServiceModel
     {
-        public string email { get; set; }
         public int id { get; set; }
         public string nomeServico { get; set; }
         public double preco { get; set; }
         public int tipoPreco { get; set; }
-
     }
 }
