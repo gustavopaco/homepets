a51455a98652881f15c28f0e1c78c7364003d7c9
Rodrigo Rocha <rodrigo@rochadigital.com>
2021-06-06 15:51:13 -0300

    Ajuste no enumerador.

--- backend/HomePetsAPI/HomePets.Domain/Enumeracao.cs
+++ backend/HomePetsAPI/HomePets.Domain/Enumeracao.cs
@@ -2,10 +2,11 @@ using System;
 
 namespace HomePets.Domain
 {
-    public enum TipoUsuario{
-        DonoPet,
-        DonoHotel,
-        Freelancer
+    public enum TipoUsuario
+    {
+        DonoPet = 1,
+        DonoHotel = 2,
+        Freelancer = 3
     }
 
 }
